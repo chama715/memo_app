3d40a13df7efb8f9f96ee963970ee976609bc63d
Naoto Takahashi <naoto0715charlie@gmail.com>
2025-05-11 22:58:54 +0900

    メモアプリ開始

---
+++ memo/memo/memoApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoApp.swift
+//  memo
+//
+//  Created by 高橋直斗 on 2025/05/11.
+//
+
+import SwiftUI
+
+@main
+struct memoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
